2ec1431b5d444436c37dacfc62a2291abc58ced3
mareadelmar <mar.ortega.v@gmail.com>
2021-09-20 14:03:46 -0300

    filter reducer structure

--- src/reducers/filterReducer.js
+++ src/reducers/filterReducer.js
@@ -2,3 +2,20 @@ export const filterReducer = (state = "all", action) => {
     console.log("action", action);
     return state;
 };
+
+
+// Gestionar el estado de las SLIDES (backoffice)
+
+export const slidesReducer = (state, action) => {
+    console.log(action)
+    return state;
+}
+
+/*
+- deberá tener una acción para renderizar el listado de los Slides en BackOffice
+- la lógica existente de peticiones a la API deberá moverse a thunks asíncronos dentro del reducer. 
+- utilizar el método createAsyncThunk de Redux Toolkit para la creación de thunks.
+*/
+
+
+
